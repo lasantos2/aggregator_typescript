a2cdda8fedfe684e84da0c94a23ecbd4517341f8
Luis <lasantos2@outlook.com>
2025-06-18 15:14:02 -0600

    New addition to scheme for feeds_follow table

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -23,3 +23,22 @@ export const feeds = pgTable("feeds", {
     .references(() => users.id, { onDelete: "cascade" })
     .notNull(),
 });
+
+export const feed_follows = pgTable("feed_follows", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  name: text("name").notNull().unique(),
+  url: text("url").notNull().unique(),
+  user_id: uuid("user_id")
+    .references(() => users.id, { onDelete: "cascade" })
+    .notNull()
+    .unique(),
+  feed_id: uuid("feed_id")
+    .references(() => feeds.id, { onDelete: "cascade" })
+    .notNull()
+    .unique(),
+});
