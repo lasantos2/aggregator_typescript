983e5ea5f82a8625a903ac9d45a3fbe7d2f3c52e
Luis <lasantos2@outlook.com>
2025-06-27 14:50:08 -0600

    Created the getPostsForUser returns latest posts using publishedAt

--- src/lib/db/queries/posts.ts
+++ src/lib/db/queries/posts.ts
@@ -1,30 +1,44 @@
-import { posts } from "../schema";
+import { sql } from "drizzle-orm";
+import { db } from "..";
+import { Post, posts } from "../schema";
 
-export async function createPost(userId: string, feedId: string) {
-  const [newPost] = await db
-    .insert(posts)
-    .values({ feedId, userId })
-    .returning();
+export async function createPost(feedId: string, postTitle: string) {
+  //  const [newPost] = await db
+  //    .insert(posts)
+  //    .values({
+  //    })
+  //    .returning();
+  //
+  //  const [result] = await db
+  //    .select({
+  //      id: feedFollows.id,
+  //      createdAt: feedFollows.createdAt,
+  //      updatedAT: feedFollows.updatedAt,
+  //      userId: feedFollows.userId,
+  //      feedId: feedFollows.feedId,
+  //      feedName: feeds.name,
+  //      userName: users.name,
+  //    })
+  //    .from(feedFollows)
+  //    .innerJoin(feeds, eq(feedFollows.feedId, feeds.id))
+  //    .innerJoin(users, eq(feedFollows.userId, users.id))
+  //    .where(
+  //      and(
+  //        eq(feedFollows.id, newFeedFollow.id),
+  //        eq(users.id, newFeedFollow.userId),
+  //      ),
+  //    );
+  //
+  return {};
+}
 
-  const [result] = await db
-    .select({
-      id: feedFollows.id,
-      createdAt: feedFollows.createdAt,
-      updatedAT: feedFollows.updatedAt,
-      userId: feedFollows.userId,
-      feedId: feedFollows.feedId,
-      feedName: feeds.name,
-      userName: users.name,
-    })
-    .from(feedFollows)
-    .innerJoin(feeds, eq(feedFollows.feedId, feeds.id))
-    .innerJoin(users, eq(feedFollows.userId, users.id))
-    .where(
-      and(
-        eq(feedFollows.id, newFeedFollow.id),
-        eq(users.id, newFeedFollow.userId),
-      ),
-    );
+export async function getPostsForUser(postLimit: number) {
 
-  return result;
+  const results = await db
+    .select()
+    .from(posts)
+    .orderBy(sql`${posts.published_at} desc nulls first`)
+    .limit(postLimit);
+
+  return results;
 }
